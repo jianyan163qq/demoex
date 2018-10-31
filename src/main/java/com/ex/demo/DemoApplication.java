3ca7d662ef84a322a2df866a5e8230453babe261
yangjianyan <yangjianyan@ppdai.com>
2018-10-31 17:45:20 +0800

    接入elk

--- src/main/java/com/ex/demo/DemoApplication.java
+++ src/main/java/com/ex/demo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.ex.demo;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @EnableApolloConfig
 @SpringBootApplication
@@ -12,5 +14,14 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+        while (true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("======================started============================");
+            log.info("started");
+        }
     }
 }
